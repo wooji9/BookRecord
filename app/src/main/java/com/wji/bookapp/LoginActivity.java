256d56d58ac49f3cb08ca4c4e3791739349dc794
Inpyo95 <sip0315@naver.com>
2021-01-29 12:14:16 +0900

    회원가입 및 로그인

---
+++ app/src/main/java/com/wji/bookapp/LoginActivity.java
@@ -0,0 +1,94 @@
+package com.wji.bookapp;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button loginButton;
+    TextView registerButton;
+    EditText idText, passwordText;
+    AlertDialog dialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        registerButton = findViewById(R.id.registerButton);
+        idText = findViewById(R.id.idText);
+        passwordText = findViewById(R.id.passwordText);
+        loginButton = findViewById(R.id.loginButton);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userID = idText.getText().toString();
+                String userPassword = passwordText.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>(){
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if(success){
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                dialog = builder.setMessage("로그인에 성공했습니다.")
+                                        .setPositiveButton("확인", null)
+                                        .create();
+                                dialog.show();
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                LoginActivity.this.startActivity(intent);
+                                finish();
+                            }else{
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                dialog = builder.setMessage("계정을 다시 확인하세요.")
+                                        .setNegativeButton("다시 시도", null)
+                                        .create();
+                                dialog.show();
+                            }
+                        }catch(Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                LoginRequest loginRequest = new LoginRequest(userID, userPassword, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
+            }
+        });
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        if(dialog != null){
+            dialog.dismiss();
+            dialog = null;
+        }
+    }
+}
