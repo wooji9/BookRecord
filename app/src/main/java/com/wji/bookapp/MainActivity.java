28974941d579458f628d9a2a8ac87c371527fed7
Inpyo95 <sip0315@naver.com>
2021-02-01 16:27:57 +0900

    메인화면 버튼액션

--- app/src/main/java/com/wji/bookapp/MainActivity.java
+++ app/src/main/java/com/wji/bookapp/MainActivity.java
@@ -1,14 +1,64 @@
 package com.wji.bookapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button homeButton, flowerButton, recordButton, profileButton;
+    LinearLayout notice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        homeButton = findViewById(R.id.homeButton);
+        flowerButton = findViewById(R.id.flowerButton);
+        recordButton = findViewById(R.id.recordButton);
+        profileButton = findViewById(R.id.profileButton);
+        notice = findViewById(R.id.notice);
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                notice.setVisibility(view.GONE);
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        /*flowerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                notice.setVisibility(view.GONE);
+                Intent intent = new Intent(MainActivity.this, FlowerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        recordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                notice.setVisibility(view.GONE);
+                Intent intent = new Intent(MainActivity.this, RecordActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                notice.setVisibility(view.GONE);
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });*/
     }
 }
